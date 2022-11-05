675c3fb22f1e4abdd573bb4bd33dd3eec52b3ba3
najeeb-yusuf <yusufnajlawal@gmail.com>
2022-11-05 20:31:39 +0100

    Index done

--- peterp/faculty/urls.py
+++ peterp/faculty/urls.py
@@ -5,5 +5,13 @@ app_name = 'faculty'
 
 urlpatterns = [
     path("",views.index,name='index'),
-    path("logout/",views.logout_user,name='logout')
+    path("logout/",views.logout_user,name='logout'),
+    path("profile/",views.profile,name='profile'),
+    path("dashboard/",views.dashboard,name='dashboard'),
+    path("semester_schedule/",views.dashboard,name='semester_schedule'),
+    path("course_overview/",views.course_overview,name='course_overview'),
+    path("sections/",views.course_overview,name='sections'),
+    path("courses/",views.course_overview,name='courses'),
+    path("allocated_grades/",views.course_overview,name='allocate_grades'),
+    path("grade_history/",views.grade_history,name='grade_history'),
 ]
--- peterp/faculty/views.py
+++ peterp/faculty/views.py
@@ -21,25 +21,139 @@ def index(request):
     else:
         token = request.session.get('token')
         # make api requests and get important data that will be cached
-        faculty_data = requests.get(URL+"/faculty",headers={'Authorization':'Bearer ' + token})
-        # concise_schedule = requests.get(URL+"/concise_schedule",headers={'Authorization':'Bearer ' + token})
-
+        faculty_data = requests.get(
+            URL+"/faculty", headers={
+                'Authorization': 'Bearer ' + token,
+                'Accept':'application/vnd.pgrst.object+json'
+                })
         # if the status_code is 401 it means the token is expired. Redirect the user to logout and create an error message
+        fac_id = faculty_data.json()['faculty_id']
+        semester_schedule = requests.get(
+            URL+f"/session?select=*,sections(*,section_times(class_dates_abbrev(abbrev),class_times(str_rep)),course:courses(*),faculty_assignment!inner(*))&sections.faculty_assignment.fac_id=eq.{fac_id}&status=eq.active&state_id=gt.2"
+            ,headers={
+                'Authorization':'Bearer ' + token,
+                'Accept':'application/vnd.pgrst.object+json'
+                })
         if faculty_data.status_code == 401:
-            messages.add_message(request,messages.WARNING,"Session expired, please login again")
+            messages.add_message(request, messages.WARNING,
+                                 "Session expired, please login again")
             return redirect(reverse('faculty:logout'))
         # for the current use case if there is no 401 error then the data is valid
         # this probably will not hold in production but suffices for now
         else:
             # cache the important data about the current use to avoid making repeated api calls
-            request.session['faculty_data'] = faculty_data.json()[0]
-            # request.session['concise_schedule'] = concise_schedule.json()
-        return render(request,"faculty_view/index.html",{
-            'faculty_data':faculty_data.json()[0],
-            # 'concise_schedule' : concise_schedule.json(),
-            })
+            request.session['faculty_data'] = faculty_data.json()
+            request.session['semester_schedule'] = semester_schedule.json()
+        role = requests.get(URL+"/get_role")
+        request.session['role'] = role.json()
+        return render(request, "faculty_view/index.html", {
+            'faculty_data': faculty_data.json(),
+            'semester_schedule' : semester_schedule.json(),
+        })
+
+
+def semester_schedule(request):
+    # if there's no student_data or profile then the user is not logged in, redirect to login
+    if not request.session.get('faculty_data') or not request.session.get('semester_schedule'):
+        return redirect(reverse('home:login'))
+    if not request.session['token']:
+        return redirect(reverse('home:login'))
+    semester_schedule = request.session.get('semester_schedule')
+    faculty_data = request.session.get('faculty_data')
+    return render(request,'faculty_view/semester_schedule.html',{
+        'semester_schedule':semester_schedule,
+        'faculty_data':faculty_data,
+    })
+
+
+
+def course_overview(request):
+    pass
+
+
+def grade_history(request):
+    pass
+
+
+def allocate_grades(request):
+    pass
+
+
+def dashboard(request):
+    pass
+
+
+def profile(request):
+    return render(request,'faculty_view/profile.html')
+        
+
+
+def sections(request):
+    if not request.session.get('student_data') or not request.session.get('concise_schedule'):
+        return redirect(reverse('home:login'))
+    if not request.session['token']:
+        return redirect(reverse('home:login'))
+    token = request.session['token']
+    r = requests.get(URL+"/sections?select=section_id,section_number,location,capacity,session(semester,year),courses(course_code,credit_hours,title),section_times(class_dates_abbrev(abbrev),class_times(str_rep)),faculty_assignment(faculty(f_name,l_name,m_name))",headers={'Authorization':'Bearer ' + token})
+    if 0 <= r.status_code - 400 < 100:
+        messages.add_message(request, messages.WARNING, r.json()['message'])
+        return redirect(reverse('home:login'))
+    else:
+        sections = r.json()
+
+
+def courses(request):
+    if not request.session.get('student_data') or not request.session.get('concise_schedule'):
+        return redirect(reverse('home:login'))
+    if not request.session['token']:
+        return redirect(reverse('home:login'))
+    token = request.session['token']
+    r = requests.get(URL+"/courses",headers={'Authorization':'Bearer ' + token})
+    if 0 <= r.status_code - 400 < 100:
+        messages.add_message(request, messages.WARNING, r.json()['message'])
+        return redirect(reverse('home:login'))
+    else:
+        courses = r.json()
+        
+
+    return render(request,'student_view/courses.html',{
+        'courses':courses,
+    })
+
+
+def overrides(request):
+    pass
+
 
 def logout_user(request):
     # delete the session data and redirect to login. This makes the system forget there's ever been a person logged in
     request.session.flush()
     return redirect(reverse('home:login'))
+
+
+def redirect_by_code(request, incoming_request, source_page):
+    """
+    This function is meant to handle error codes.
+    Any 4** code will be received by this function and the appropriate page will be rendered
+
+    I know that this is a bit of an unsavory way of implementing this 
+    but it's the fastest and I don't have to use my brain
+    """
+    code = incoming_request.status_code
+
+    if code - 400 >= 0 and code - 400 <= 99:
+        messages.add_message(request, messages.WARNING,
+                             incoming_request.json()['message'])
+        if code == 401:
+            return redirect(reverse('home:login'))
+        elif code == 403:
+            # 403 forbidden means the user is not allowed to access this page
+            return render(request, 'faculty_view/forbidden.html')
+        elif code == 404:
+            return render(request, 'faculty_view/notfound404.html')
+        else:
+            # if the status code is 400 it means it's a bad request so just login again.
+            return redirect(reverse(source_page))
+    else:
+        messages.add_message(request, messages.SUCCESS, "Success")
+        return redirect(reverse(source_page))
