2b665ddf25a0e1fb382363d5492e35ddb30949d6
najeeb-yusuf <yusufnajlawal@gmail.com>
2022-11-26 21:14:28 +0100

    Student messages done

--- peterp/faculty/views.py
+++ peterp/faculty/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.urls import reverse
 
 from django.http import HttpResponseRedirect
+
 from student.models import StudentMessage
 # Create your views here.
 URL = 'http://aun-erp-api.herokuapp.com'
